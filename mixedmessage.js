9019b17f8a2e0e3482d6389deda4df3d1d2ac546
JorritvdP <jvdplaats@hotmail.com>
2022-04-21 14:36:53 +0200

    first try

--- mixedmessage.js
+++ mixedmessage.js
@@ -2,11 +2,11 @@
 
 // creating object to store messages
 
-const start = ['Do not forget: ', 'Remember: ', 'Some insparation: '];
+/*const start = ['Do not forget: ', 'Remember: ', 'Some insparation: '];
 const quote = ['Change the world by being yourself.', 'Every moment is a fresh beginning.', 'Die with memories, not dreams.', 'Aspire to inspire before we expire.', 'What we think, we become.', 
                 'If you tell the truth you do not have to remember anything.', 'Simplicity is the ultimate sophistication.', 'Everything you can imagine is real.', 'Whatever you do, do it well.', 
                 'Tough times never last but tough people do.'];
-const end = ['Today is a good day!', 'Tomorrow will be better!', 'Yesterday is gone!', 'Live today!', 'Embrace this day!'];
+const end = ['Today is a good day!', 'Tomorrow will be better!', 'Yesterday is gone!', 'Live today!', 'Embrace this day!'];*/
 
 
 let mixedMessages = {
@@ -30,3 +30,18 @@ const generator = () => {
 };
 
 generator()
+
+// random sentence selector
+
+const sentence = {
+    wordOne: ['I', 'Love', 'You'],
+    wordTwo: ['Love', 'is', 'Hate'],
+    wordThree: ['Me', 'all around', 'You']
+};
+
+const test = () => {
+    let result = [];
+    let count = 0;
+    let j= Math.floor(Math.random()*3);
+    
+}
